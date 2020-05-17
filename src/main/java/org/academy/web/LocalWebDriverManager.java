03b4d6f945010e6b650b9f701fbbaca2f97c7eae
TetianaKulish <teti98kulish@gmail.com>
2020-05-18 01:07:22 +0300

    Get hash

---
+++ src/main/java/org/academy/web/LocalWebDriverManager.java
@@ -0,0 +1,32 @@
+package org.academy.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.academy.MainConfig;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class LocalWebDriverManager {
+
+	public static synchronized WebDriver getWebDriver() {
+		return getWebDriver(MainConfig.getBrowser());
+	}
+
+	public static WebDriver getWebDriver(String browser) {
+		switch (browser) {
+			default:
+			case "chrome":
+				ChromeOptions options = new ChromeOptions();
+				String os = System.getProperty("os.name").toLowerCase().substring(0, 3);
+				String chromeBinary = "src/main/resources/chromedriver" + (os.equals("win") ? "77.exe" : "");
+				System.setProperty("webdriver.chrome.driver", chromeBinary);
+				WebDriver chromeWebDriver = new ChromeDriver(options);
+				chromeWebDriver.manage().timeouts().implicitlyWait(20000, TimeUnit.MILLISECONDS);
+				chromeWebDriver.manage().window().maximize();
+				return chromeWebDriver;
+		}
+	}
+}
